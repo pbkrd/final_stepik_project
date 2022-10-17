6ab7207ad4c3d46a4186680506e71e31b2b05cf5
Petr <baryshev.petr@yandex.ru>
2022-10-17 18:15:50 +0300

    Added pages

--- test_main_page.py
+++ test_main_page.py
@@ -1,7 +1,8 @@
-from selenium.webdriver.common.by import By
+from .pages.main_page import MainPage
+
 
 def test_guest_can_go_to_login_page(browser, lang_option):
-    link = f"http://selenium1py.pythonanywhere.com/{lang_option}/"
-    browser.get(link)
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
+    link = f"http://selenium1py.pythonanywhere.com/{lang_option}"
+    page = MainPage(browser, link)  
+    page.open()
+    page.go_to_login_page()
